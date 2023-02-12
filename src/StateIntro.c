93f4b8dc1c00e15cbb844c81673e6710fe4cb07a
zd <zheladev@gmail.com>
2023-02-12 16:45:47 +0100

    Add title screen tileset, map

--- src/StateIntro.c
+++ src/StateIntro.c
@@ -5,16 +5,10 @@
 #include "ZGBMain.h"
 #include "Print.h"
 
-IMPORT_TILES(font);
 IMPORT_MAP(title8);
 
 void START() {
 	INIT_BKG(title8);
-
-	INIT_FONT(font, PRINT_BKG);
-	PRINT(0, 13, "PRESS START");
-	PRINT(1, 16, "TEST TEXT TEXT");
-	PRINT(5, 17, "THIS ONE OVERFLOWS");
 }
 
 void UPDATE() {
