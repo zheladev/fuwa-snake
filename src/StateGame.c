d0e4faf7d613f63235d53abadd9d26f738d24a4a
zd <zheladev@gmail.com>
2023-02-12 20:36:00 +0100

    Implement grid movement.

--- src/StateGame.c
+++ src/StateGame.c
@@ -15,7 +15,7 @@ IMPORT_TILES(font);
 
 void START() {
 	INIT_CONSOLE(font, 1);
-	scroll_target = SpriteManagerAdd(SpritePlayer, 50, 50);
+	scroll_target = SpriteManagerAdd(SpritePlayer, 64, 64);
 	SpriteManagerAdd(SpriteTab, 16, 16);
 	InitScroll(BANK(map), &map, collision_tiles, 0);
 
@@ -24,15 +24,15 @@ void START() {
 void UPDATE() {
 	UINT8 i;
 	Sprite* spr;
-	SPRITEMANAGER_ITERATE(i, spr) {
-		if (spr->type == SpriteTab) {
-			//DPRINT_POS(0, 0);
-			//DPrintf("x:%d y:%d  ", spr->x, spr->y);
-		} else if (spr->type == SpritePlayer) {
-			//DPRINT_POS(0, 1);
-			//DPrintf("x:%d y:%d  ", spr->x, spr->y);
-		}
-	}
+	//SPRITEMANAGER_ITERATE(i, spr) {
+		//if (spr->type == SpriteTab) {
+		//	DPRINT_POS(0, 0);
+		//	DPrintf("x:%d y:%d  ", spr->x, spr->y);
+		//} else if (spr->type == SpritePlayer) {
+		//	DPRINT_POS(0, 1);
+		//	DPrintf("x:%d y:%d  ", spr->x, spr->y);
+		//}
+	//}
 	DPRINT_POS(0, 0);
 	DPrintf("score:%d  ", score);
 }
